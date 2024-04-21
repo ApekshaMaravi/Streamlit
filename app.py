acd4a72b813eac53ade69264b3f0d858a1660c19
ApekshaMaravi <107713839+ApekshaMaravi@users.noreply.github.com>
2024-04-21 15:38:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,41 @@
 import streamlit as st
-
 st.write("""
-# Largest number
+# Largest number 
 
-This app tells the largest number
+This app predicts the largest of the given 3 numbers
 """)
-#Get Input
+st.header('User input numbers')
+num1 = st.number_input('FIRST NUMBER')
+num2 = st.number_input('SECOND NUMBER')
+num3 = st.number_input('THIRD NUMBER')
 
-st.header('Enter the numbers')
-
-def user_input_features():
-    first_number = st.number_input("First number")
-    second_number = st.number_input("Second number")
-    third_number = st.number_input("Third number")
-    return max(first_number,second_number,third_number)
-
-x=user_input_features()
-st.subheader('The maximum among three numbers is:')
-st.write(x)
+  # data = {'FIRST NUMBER' : num1,
+  #         'SECOND NUMBER' : num2,
+  #         'THIRD NUMBER' : num3
+  #         }
+ans = 0
+if(num1>num2 and num1>num3) :
+  ans = num1
+elif(num2 > num1 and num2 > num3) :
+  ans = num2
+elif(num3>num1 and num3>num2):
+  ans = num3
+elif(num1==num2):
+  if(num1 > num3):
+    ans = num1
+  else:
+    ans = num3
+elif(num2==num3):
+  if(num2 > num1):
+    ans = num2
+  else:
+    ans = num1
+elif(num1==num3):
+  if(num1 > num2):
+    ans = num1
+  else:
+    ans = num2
+elif(num1==num2==num3):
+  ans = num1
+st.subheader("The largest number")
+st.write(ans)
